9e6ccbfb2295c7dd41fcebf08d7fe748c6ac05ee
Julien <julien.crypt@gmail.com>
2021-04-06 17:28:17 +0200

    provisoire

--- contracts/AcroDatabase.sol
+++ contracts/AcroDatabase.sol
@@ -3,44 +3,18 @@ pragma solidity ^0.8.0;
 
 contract AcroDatabase is AcroActors {
 
-   // TODO: Affiner et optimiser les types de données ci dessous
-   // TODO: Arbitrer la manière dont on empile l'historique de l'état d'une zone
-   struct ZoneRelle {
-      // Identifiant de la zone = Index dans le tableau 
-      uint date_prise_photo;
-      string fichier;
-      uint latitude;
-      uint longitude;
-      uint profondeur;
-      uint pourcentage_vivant;
-      uint taille;
-      uint espece;
-      uint coral_species;
-      uint state;		// Faire un enum. Une zone peut aussi avoir un état "n'existe plus"
+   // Regroupement des "100" tokens associés à une même zone
+   uint cpt_lagoon_id;
 
-      address acteur;	// Acteur qui a référencé la zone (pour statistiques)
-			// Pour connaitre l'individu qui a adpoté une part de la zone, il faut regarder l'affectation via son NFT "Lag"
-   }
-
-   struct ItemJeu {
-      uint8 x;
-      uint8 y;
-      uint8 type_item;
-   }
-
-   struct ZoneVirtuelleJeu {
-      ItemJeu[] items_jeu;
-      // Pour connaitre l'individu qui possède cette zone virtuelle, il faut regarder l'affectation via son NFT "Lag"
-   }    
-
-   ZoneRelle[] zones_reelles;
-   ZoneVirtuelleJeu[] zones_virtuelles_jeu;
-
-   function new_zone(/* infos sur la zone */) public {	// Argument: voir si il est possible de passer une structure
+   function new_real_zone(/* infos sur la zone */) public {	// Argument: voir si il est possible de passer une structure
       require(actors_whitelist[msg.sender]==true);
+
+      // minter "100" lagoons
+
+      lagoon_id++;
    }
 
-   function update_zone_state(/* infos sur la zone */) public {
+   function update_real_zone_state(/* infos sur la zone */) public {
       require(actors_whitelist[msg.sender]==true);
    }
 }
--- contracts/Lagoon.sol
+++ contracts/Lagoon.sol
@@ -3,16 +3,52 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+ERC1155: Stocker 1 seul jeu de caractéristiques mais avoir 100 exemplaires
+
 contract Lagoon is ERC721 {
-  bool has_zone_reelle;
-  uint zone_reelle_id;
 
-  bool has_zone_virtuelle_jeu;
-  uint zone_virtuelle_jeu_id;
+   struct RealZone {
+      // Identifiant de la zone = Index dans le tableau 
+      // Voir opensea
+      // uint lagoon_id;
+      // uint quantite = 100;
 
-  constructor() ERC721("LAGOON", "LAG") {
+      uint date_prise_photo;
+      string fichier;
+      uint latitude;
+      uint longitude;
+      uint profondeur;
+      uint pourcentage_vivant;
+      uint taille;
+      uint espece;
+      uint coral_species;
+      uint state;		// Faire un enum. Une zone peut aussi avoir un état "n'existe plus"
 
-  }
+      address acteur;	// Acteur qui a référencé la zone (pour statistiques)
+			// Pour connaitre l'individu qui a adpoté une part de la zone, il faut regarder l'affectation via son NFT "Lag"
+   }
+
+   struct VirtualItem {
+      uint8 x;
+      uint8 y;
+      uint8 item_type;
+   }
+
+   struct VirtuaZone {
+      string zone_name;
+      VirtualItem[] items;
+   }
+
+   RealZone real_zone;
+   VirtuaZone virtual_zone;
+
+   constructor() ERC721("LAGOON", "LAG") {
+
+   }      
 
   // TODO: Redéfinir les mécanismes de transfer pour le prélèvement de frais
+
+  function merge_token() {
+     // TODO: fusion de tokens
+  } 
 }
