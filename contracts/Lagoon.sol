3f4489877a85d259123c71476fb35d7d15d00ade
melanie971 <melanie.beguer@gmail.com>
2021-04-06 17:55:35 +0100

    english version

--- contracts/Lagoon.sol
+++ contracts/Lagoon.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-ERC1155: Stocker 1 seul jeu de caractéristiques mais avoir 100 exemplaires
+// Change ERC721 to ERC1155: Stocker 1 seul jeu de caractéristiques mais avoir 100 exemplaires
 
 contract Lagoon is ERC721 {
 
@@ -13,18 +14,17 @@ contract Lagoon is ERC721 {
       // uint lagoon_id;
       // uint quantite = 100;
 
-      uint date_prise_photo;
-      string fichier;
-      uint latitude;
-      uint longitude;
-      uint profondeur;
-      uint pourcentage_vivant;
-      uint taille;
-      uint espece;
-      uint coral_species;
-      uint state;		// Faire un enum. Une zone peut aussi avoir un état "n'existe plus"
+      uint date_prise_photo; //change type?
+      string jpeg_file; //link toward image
+      uint latitude; //latitude of coral reef area
+      uint longitude; //longitude of coral reef area
+      uint depth; //in meters, classes? in that case it would be string
+      
+      uint nb_coral_species; //number of coral reef species
+      uint nb_dead_corals; // in order to calculate a percentage of alive coral?
+      uint heath_state;		// TO BE DEFINED 0-very bad health -> 5 top reef! Faire un enum. Une zone peut aussi avoir un état "n'existe plus"
 
-      address acteur;	// Acteur qui a référencé la zone (pour statistiques)
+      address actor;	// Acteur qui a référencé la zone (pour statistiques)
 			// Pour connaitre l'individu qui a adpoté une part de la zone, il faut regarder l'affectation via son NFT "Lag"
    }
 
@@ -34,7 +34,7 @@ contract Lagoon is ERC721 {
       uint8 item_type;
    }
 
-   struct VirtuaZone {
+   struct VirtualZone {
       string zone_name;
       VirtualItem[] items;
    }
