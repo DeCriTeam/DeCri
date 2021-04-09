5b3a7a3f94c43c77360370ddadee8b21957f6dfa
Julien <julien.crypt@gmail.com>
2021-04-09 08:17:41 +0000

    Laggon

--- contracts/AcroDatabase.sol
+++ contracts/AcroDatabase.sol
@@ -3,17 +3,13 @@ pragma solidity ^0.8.0;
 
 contract AcroDatabase is AcroActors {
 
-   // Regroupement des "100" tokens associés à une même zone
-   uint cpt_lagoon_id;
-
+   // A deplacer dans Lagoon ?
    function new_real_zone(/* infos sur la zone */) public {	// Argument: voir si il est possible de passer une structure
       require(actors_whitelist[msg.sender]==true);
 
-      // minter "100" lagoons
-
-      lagoon_id++;
    }
 
+   // A deplacer dans Lagoon ?
    function update_real_zone_state(/* infos sur la zone */) public {
       require(actors_whitelist[msg.sender]==true);
    }
--- contracts/Lagoon.sol
+++ contracts/Lagoon.sol
@@ -1,18 +1,11 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-ERC1155: Stocker 1 seul jeu de caractéristiques mais avoir 100 exemplaires
-
-contract Lagoon is ERC721 {
-
+contract Lagoon is ERC1155 {
    struct RealZone {
       // Identifiant de la zone = Index dans le tableau 
-      // Voir opensea
-      // uint lagoon_id;
-      // uint quantite = 100;
-
       uint date_prise_photo;
       string fichier;
       uint latitude;
@@ -42,13 +35,28 @@ contract Lagoon is ERC721 {
    RealZone real_zone;
    VirtuaZone virtual_zone;
 
-   constructor() ERC721("LAGOON", "LAG") {
 
+   constructor() ERC1155("https://ipfs.io/")   //("LAGOON", "LAG") {
+      
    }      
 
-  // TODO: Redéfinir les mécanismes de transfer pour le prélèvement de frais
-
-  function merge_token() {
+   function merge_token() {
      // TODO: fusion de tokens
-  } 
+   } 
+
+   function lock(uint token_id,uint amount) {
+      // TODO
+   }
+
+   function unlock(uint token_id, uint amount) private {
+      // TODO
+   }
+
+   function buy_virtual_item(uint token_id,uint item_type_id) payable {
+      // TODO
+   }
+
+   function add_virtual_item(uint token_id, uint item_id, uint8 x, uint8 y) {
+      // TODO
+   }
 }
