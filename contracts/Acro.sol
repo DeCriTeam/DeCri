a6ef177922b1a5a6e17b98912f57e72109d0abee
melanie971 <melanie.beguer@gmail.com>
2021-04-10 23:10:07 +0100

    Events added

--- contracts/Acro.sol
+++ contracts/Acro.sol
@@ -4,16 +4,27 @@ pragma solidity 0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract Acro is ERC20, Ownable {
+   
+   using SafeMath for uint;
+
     constructor(uint256 initialSupply) ERC20("Acropora Token", "ACRO") {
         _mint(/* msg.sender */ address(this), 10000000000000000000000 /* initialSupply*/ );
     }
     
+   //List of events
+   event buyingAcro(address msgsender, uint256 amount);
+   event donatingAcro(address msgsender, uint256 amout);
+   event withdrawal(address otheradress, uint256 amount);
+
     function tmp_buy_acro() external payable {
        require(msg.value>0);
        // _mint(msg.sender, msg.value*20);
        _transfer( address(this), msg.sender, msg.value*20);		// TODO: Handle Error
+       emit buyingAcro(msg.sender, msg.value);
+
     }
 
     function get_ether_balance_of_this_contract() external view returns (uint) {
@@ -24,7 +35,7 @@ contract Acro is ERC20, Ownable {
        return balanceOf(address(this));
     }
 
-   // Does not work on Ganache: wrong user eth balance value
+   // Does not work on Ganache: wrong user eth balance value. Ganache bug or what ?
     function get_ether_balance_of_sender() external view returns (uint) {
        return msg.sender.balance;
     }
@@ -33,13 +44,16 @@ contract Acro is ERC20, Ownable {
        return balanceOf(msg.sender);
     }
 
-    function acro_donation(uint amount) external {
-       transfer(address(this), amount); 
+    function acro_donation(uint256 amount) external {
+       transfer(address(this), amount);
+       emit donatingAcro(msg.sender, amount);
     }
 
-    function withdraw_ether(uint amount) external payable onlyOwner {
-      require(address(this).balance >= amount);
-      msg.sender.transfer(amount);   
+    function withdraw_ether(uint256 amount) external payable onlyOwner {
+      require(address(this).balance >= amount, "account balance is too low");
+      msg.sender.transfer(amount);
+      emit withdrawal(msg.sender, amount);
     }
+
 }
 
