c71609825a55367903ae6fa0dabd60f4af004cbd
melanie971 <melanie.beguer@gmail.com>
2021-04-13 14:47:57 +0100

    Functionnal 1st version

--- contracts/AcroActors.sol
+++ contracts/AcroActors.sol
@@ -1,54 +1,71 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
-// pragma experimental ABIEncoderV2; // A voir si on l'utilise (pour récupérer tableau dans front)
+import "@openzeppelin/contracts/access/Ownable.sol";
+pragma experimental ABIEncoderV2; // A voir si on l'utilise (pour récupérer tableau dans front)
 
-contract AcroActors {
-   // A voir: un seul tableau: Quand on blacklist on supprime l'entrée (ou mise à false)
-   mapping(address => bool) public actors_whitelist;
-   mapping(address => uint) public actors_score_whitelist;
-   mapping(address => mapping(address => bool)) public already_vote;
-
-   // mapping(address => bool) actors_blacklist;
-
-   function is_actor(address addr) external view returns (bool) {
-     return actors_whitelist[addr];
-   }
-
-   constructor() {
-     actors_whitelist[msg.sender] = true;
-   } 
-
-/*
+contract AcroActors is Ownable {
+   
+   //struc limited to 12 variables apparently ? 
    struct Actor {
-      uint id; //!!to be added below & !!to be regrouped by var.cat. for optim
       bool isRegistered;
-      bool isValidated;
-      uint nbMintedArea; // to be put in a mapping instead?
-      uint voteCount; //number of votes for this actor
-      string dateOfRegistration; // ? do we put that - CHECK variable type
+      uint id; //!!to be added below & !!to be regrouped by var.cat. for optim
+      // uint nbMintedArea; // to be put in a mapping instead?
       
+      string dateOfRegistration; // ? do we put that - CHECK variable type
       string actorName;
       string country;
       uint latCenter;
       uint longCenter;
       uint yearOfCreation; // year of birth of NGO ?
       string email; //type to be checked
-      string actorType; // individual, NGO, diving club
-      
+      string actorType; // individual, NGO, diving club      
    }
 
+   uint actorCount = 0; //to keep track of the number of actors and to generate a unique ID
+
    mapping(address => Actor) public RegisteredActors;
+   mapping(address => bool) public actors_whitelist; //id instead?
+   mapping(address => uint) public actors_score_whitelist;
+
+   mapping(address => uint) public actors_registred_id; //Not needed
+   mapping(address => bool) public isRegistered;
+   mapping(address => mapping(address => bool)) public already_vote;
+
+   //TO ADD: Nb of minted area - mapping
+
    address[] public actors;
+   
+   constructor() {
+     actors_whitelist[msg.sender] = true;
+   } 
 
-   uint public actorCount; //to keep track of the number of actors and to generate a unique ID
+   function is_actor(address addr) external view returns (bool) { //confusing iswhitelisted instead, change later because of Front!
+     return actors_whitelist[addr];
+   }
 
+   function is_registered(address addr) external view returns (bool) {
+      return isRegistered[addr];
+   }
+
+   function get_actor_id(address addr) external view returns (uint) {
+      return actors_registred_id[addr];
+   }
+
+   function get_actors_score_whitelist(address addr) external view returns (uint) {
+      return actors_score_whitelist[addr];
+   }
+
+ ////To DO:  get actors informations
+ 
+// /*
+  
    //List of events
    event ActorRegistered(address actorAddress);
-   event ActorDeleted(address actorAddress);
-   event votedEvent(uint indexed actor_id); //several actors at once?
+   event ActorDeleted(address actorAddress); // à conserver
+   event votedEvent(address actorAddress); //several actors at once?
+   event ActorIsValided(address actorAddress);
 
-
-   //NOT FINISHED: adding new actors
+   // To add new actors: anybody can add a new actor (one at once)
    function add_new_actor(address _address,
                           string memory _actorName,
                           string memory _country,
@@ -56,103 +73,51 @@ contract AcroActors {
                           uint _longCenter,
                           uint _yearOfCreation,
                           string memory _email, //type to be checked
-                          string memory _actorType
-                          
+                          string memory _actorType,
+                          string memory date
                           ) public {
-      require(!RegisteredActors[_address].isRegistered != true,"This address is already registered");
-      RegisteredActors[actorCount] = Actor(actorCount,true,false,0,0,date, _actorName, _country,_latCenter, _longCenter,  _yearOfCreation,_email,_actorType); //DATE A VOIR
+      require(RegisteredActors[_address].isRegistered != true,"This address is already registered"); // Doublon à voir
+      require(isRegistered[_address] != true, "This address is already registered");
+      RegisteredActors[_address] = Actor(true, actorCount,date, _actorName, _country,_latCenter, _longCenter,  _yearOfCreation,_email,_actorType); //DATE A VOIR
       actors.push(_address);
+      actors_registred_id[_address] = actorCount;
+      isRegistered[_address] = true;
       actorCount++;
       emit ActorRegistered(_address);
    }
 
-   //To get actors informations: 
-   function get_actors() external view returns (string[] memory,
-                                                string[] memory,
-                                                string[] memory,
-                                                string[] memory,
-                                                uint[] memory,
-                                                uint[] memory,
-                                                uint[] memory,
-                                                uint[] memory,
-                                                uint[] memory,
-                                                bool[] memory
-                                                ){
-      string[] memory actorNames = new string[](actorCount);
-      string[] memory countries = new string[](actorCount);
-      string[] memory emails = new string[](actorCount);
-      string[] memory actorTypes = new string[](actorCount);
-
-      uint[] memory voteCounts = new uint[](actorCount);
-      uint[] memory latCenters = new uint[](latCenter);
-      uint[] memory longCenters = new uint[](longCenter);
-      uint[] memory yearsOfCreation = new uint[](yearOfCreation);
-      uint[] memory nbMintedAreas = new uint[](nbMintedArea);
-      bool[] memory isValidateds = new uint[](isValidated);
-
-
-      for(uint i = 0; i < actorCount; i++) {
-         actorNames[i] = RegisteredActors[i].actorName;
-         countries[i] = RegisteredActors[i].country;
-         emails[i] = RegisteredActors[i].email;
-         actorTypes[i] = RegisteredActors[i].actorType;
-
-         voteCounts[i] = RegisteredActors[i].voteCount;
-         latCenters[i] = RegisteredActors[i].voteCount;
-         longCenters[i] = RegisteredActors[i].voteCount;
-         yearsOfCreation[i] = RegisteredActors[i].voteCount;
-         isValidateds[i] = RegisteredActors[i].voteCount;
-         nbMintedAreas[i] = RegisteredActors[i].voteCount;
-      }
-      
-      return (actorNames, countries, emails, actorTypes,voteCounts,latCenters,longCenters,yearsOfCreation,isValidateds,nbMintedAreas);
-   }
-
-
-
-
-   // TODO:
-   // Mécanisme de vote avec proposals sous forme de texte: TO BE DISCUSSED
-   // Mécanisme de vote permettant l'acceptation d'un nouvel acteur
-
-   //Voting for a new actor
-   //TO DO: can only vote once for an actor
-   //TO DO: vote for multiple actors at once
-   function votingForActor (uint actor_id) external 
+  
+   // Voter pour participer à la validation d'un acteur
+   // Note: an actor can only vote once for an actor. see require already_vote...
+   // //TO DO: THAT ACTOR MUST BE STAKING SOME ACROS to vote, need to import Acro contract for that
+   
+   function votingForActor(address actor_address) external 
    { 
-      require(RegisteredActors[msg.sender].isRegistered == true);
-      require(actor_id >=0 && actor_id <= actorCount-1);
-      RegisteredActors[_address].voteCount++; //actor_id?
-      //to be done
-      emit votedEvent(actor_id);
+      require(RegisteredActors[msg.sender].isRegistered == true); //The user must be registered to vote
+      require(RegisteredActors[actor_address].isRegistered == true); //not possible to vote for a non-registered actor
+      require(msg.sender != actor_address); // a user cannot vote for himself
+      actors_score_whitelist[actor_address]++;
+      emit votedEvent(actor_address);
    }
 
 
-   //Function to validate an actor. Number of votes to be defined
-   function validateActor() public {
-      require(RegisteredActors[msg.sender].isRegistered == true);
-      if (RegisteredActors[msg.sender].voteCount >=10) {
-         RegisteredActors[msg.sender].isValidated = true;
+   //Function to validate an actor. Number of votes to be defined - Admin only
+   // TO DO: validate several actor at once
+   function validateActor(address actor_address) public onlyOwner {
+      require(RegisteredActors[actor_address].isRegistered == true); //Only registered Actors can be validated
+      
+      if (actors_score_whitelist[actor_address] > 2) { // !!! NUMBER TO BE SPECIFIED, set low here for testing purpose. FUNCTION?
+         actors_whitelist[actor_address] = true;
+         emit ActorIsValided(actor_address);
       } 
    }
 
 
 
-   //TO DO: see with Julien, what do we want here?
-   function new_actor_request() public {
+   //
+   
 
-   }
-
-   function approve_actor_request() public {
-      require(actors_whitelist[msg.sender]==true);
-      // Si le nb d'approve > seuil -> ajout à la whitelist + retrait blacklist
-      // Offre de l'acro pour compenser les frais de gas
-   }
-
-   function blacklist_request() public {
-      require(actors_whitelist[msg.sender]==true);
-      // Si le nb de requests > seuil -> ajout à la blacklist
-   }
-*/
+   
+// */
 }
 
