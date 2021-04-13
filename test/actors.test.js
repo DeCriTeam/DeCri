6191fea56b036828957d6fa5cc8a6c41e54fba50
melanie971 <melanie.beguer@gmail.com>
2021-04-13 15:49:35 +0100

    test actors Ok without ACRO staking condition

--- test/actors.test.js
+++ test/actors.test.js
@@ -2,6 +2,7 @@
 const { BN, expectEvent, expectRevert, ether } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
 const AcroActors = artifacts.require('AcroActors');
+const AcroContract = artifacts.require('Acro');
 
 contract('AcroActors', async accounts => {
 
@@ -12,9 +13,10 @@ contract('AcroActors', async accounts => {
 
     beforeEach('setup contract for each test case', async () => {
         acroActorsInstance = await AcroActors.new({from: owner});
+        acroInstance = await AcroContract.new({from: owner});
     });
 
-    console.log(user1,user2,user3,user4, user5);
+    // console.log(user1,user2,user3,user4, user5);
 
     it("The owner is in the whitelist", async() => {
         let res = await acroActorsInstance.is_actor(owner);
@@ -24,21 +26,13 @@ contract('AcroActors', async accounts => {
     });
 
     it("A user can register himself as an actor, and other actors", async() => {
-        actorName = "Manta Diving Club";
-        country = "US";
-        lat = 10;
-        long = 20;
-        yearOfCreation = 2020;
-        email = "mantaclub@gmail.com";
-        actortype = "Diving Club";
-        dateE = "11/04/2021";
-         
-        let newactor = await acroActorsInstance.add_new_actor(user1,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
+             
+        let newactor = await acroActorsInstance.add_new_actor(user1,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
         let res1 = await acroActorsInstance.get_actor_id(user1);
         // console.log(res1.toString());
         expect(res1.toString()).to.equal('0');
 
-        let newactor2 = await acroActorsInstance.add_new_actor(user2,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
+        let newactor2 = await acroActorsInstance.add_new_actor(user2,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
         let res2 = await acroActorsInstance.get_actor_id(user2);
         // console.log(res2.toString());
         expect(res2.toString()).to.equal('1');
@@ -54,33 +48,17 @@ contract('AcroActors', async accounts => {
 
     // Cas à prévoir: si un acteur possède plusieurs wallets?
     it("A user cannot register an actor (address!) already registered", async() => {
-        actorName = "Manta Diving Club";
-        country = "US";
-        lat = 10;
-        long = 20;
-        yearOfCreation = 2020;
-        email = "mantaclub@gmail.com";
-        actortype = "Diving Club";
-        dateE = "11/04/2021";
-         
-        let newactor = await acroActorsInstance.add_new_actor(user1,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
-        await expectRevert.unspecified(acroActorsInstance.add_new_actor(user1,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1}));
+           
+        let newactor = await acroActorsInstance.add_new_actor(user1,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
+        await expectRevert.unspecified(acroActorsInstance.add_new_actor(user1,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1}));
     });
 
 
 
     it("A registered actor can vote for another registered actor", async() => {
-        actorName = "Manta Diving Club";
-        country = "US";
-        lat = 10;
-        long = 20;
-        yearOfCreation = 2020;
-        email = "mantaclub@gmail.com";
-        actortype = "Diving Club";
-        dateE = "11/04/2021";
-
-        let registeredactor1 = await acroActorsInstance.add_new_actor(user1,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
-        let registeredactor2 = await acroActorsInstance.add_new_actor(user2,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
+        
+        let registeredactor1 = await acroActorsInstance.add_new_actor(user1,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
+        let registeredactor2 = await acroActorsInstance.add_new_actor(user2,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
 
         let res = await acroActorsInstance.votingForActor(user2, {from: user1});
         await expectEvent(res, "votedEvent", {actorAddress: user2}, "Voted event incorrect");
@@ -94,16 +72,7 @@ contract('AcroActors', async accounts => {
     
     it("A non-registered actor cannot vote", async() => {
 
-        actorName = "Manta Diving Club";
-        country = "US";
-        lat = 10;
-        long = 20;
-        yearOfCreation = 2020;
-        email = "mantaclub@gmail.com";
-        actortype = "Diving Club";
-        dateE = "11/04/2021";
-
-        await acroActorsInstance.add_new_actor(user1,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
+        await acroActorsInstance.add_new_actor(user1,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
         await expectRevert.unspecified(acroActorsInstance.votingForActor(user1, {from: user2})); //
 
         // let checkres = await acroActorsInstance.get_actors_score_whitelist(user1)
@@ -113,49 +82,21 @@ contract('AcroActors', async accounts => {
 
     
     it("A registered actor cannot vote for himself", async() => {
-
-        actorName = "Manta Diving Club";
-        country = "US";
-        lat = 10;
-        long = 20;
-        yearOfCreation = 2020;
-        email = "mantaclub@gmail.com";
-        actortype = "Diving Club";
-        dateE = "11/04/2021";
-
-        await acroActorsInstance.add_new_actor(user1,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
+        await acroActorsInstance.add_new_actor(user1,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
         await expectRevert.unspecified(acroActorsInstance.votingForActor(user1, {from: user1})); //
         
     });
 
     it("A registered actor cannot vote for a non registered actor", async() => {
-
-        actorName = "Manta Diving Club";
-        country = "US";
-        lat = 10;
-        long = 20;
-        yearOfCreation = 2020;
-        email = "mantaclub@gmail.com";
-        actortype = "Diving Club";
-        dateE = "11/04/2021";
-
-        await acroActorsInstance.add_new_actor(user1,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
+        await acroActorsInstance.add_new_actor(user1,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
         await expectRevert.unspecified(acroActorsInstance.votingForActor(user2, {from: user1})); //
         
     });
 
     it("A registered actor cannot vote twice (or more) for the same actor", async() => {
-        actorName = "Manta Diving Club";
-        country = "US";
-        lat = 10;
-        long = 20;
-        yearOfCreation = 2020;
-        email = "mantaclub@gmail.com";
-        actortype = "Diving Club";
-        dateE = "11/04/2021";
-
-        await acroActorsInstance.add_new_actor(user1,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
-        await acroActorsInstance.add_new_actor(user2,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
+       
+        await acroActorsInstance.add_new_actor(user1,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
+        await acroActorsInstance.add_new_actor(user2,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
         await acroActorsInstance.votingForActor(user2, {from: user1});
         await expectRevert.unspecified(acroActorsInstance.votingForActor(user2, {from: user1}));
         
@@ -164,20 +105,11 @@ contract('AcroActors', async accounts => {
     //owner can validate actor
     it("Owner can validate an actor", async() => {
 
-        actorName = "Manta Diving Club";
-        country = "US";
-        lat = 10;
-        long = 20;
-        yearOfCreation = 2020;
-        email = "mantaclub@gmail.com";
-        actortype = "Diving Club";
-        dateE = "11/04/2021";
-
         //registering actors
-        await acroActorsInstance.add_new_actor(user1,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
-        await acroActorsInstance.add_new_actor(user2,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
-        await acroActorsInstance.add_new_actor(user3,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
-        await acroActorsInstance.add_new_actor(user4,actorName,country,lat,long,yearOfCreation,email, actortype,dateE, {from: user1});
+        await acroActorsInstance.add_new_actor(user1,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
+        await acroActorsInstance.add_new_actor(user2,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
+        await acroActorsInstance.add_new_actor(user3,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
+        await acroActorsInstance.add_new_actor(user4,"MDC","US",10,20,2020,"email@", "diving club","13/04/2021", {from: user1});
 
         //the 3 registered actors vote for user3
         await acroActorsInstance.votingForActor(user3, {from: user1});
