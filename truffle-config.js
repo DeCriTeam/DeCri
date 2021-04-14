bdc07f7c38e3fcf1d0cb9015a73581d09ecfcb32
melanie971 <melanie.beguer@gmail.com>
2021-04-14 09:54:02 +0100

    config with ganache, ropsten and rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
+require('dotenv').config();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,10 +10,21 @@ module.exports = {
     develop: {
       port: 8545
     },
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
     ropsten2: {
-      provider: function() { return new HDWalletProvider(`${process.env.MNEMONIC}`, `${process.env.INFURA_URL}`) },
+      provider: function() { return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://ropsten.infura.io/v3/${process.env.INFURA_URL}`) },
       network_id: 3
-    }
+    },
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://rinkeby.infura.io/v3/${process.env.INFURA_ID}`)
+      },
+      network_id: 4
+    },
   },
   compilers: {
     solc: {
